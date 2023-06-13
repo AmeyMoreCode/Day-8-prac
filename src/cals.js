0fe513dc60520ea19df685763023c53e8f0fbdce
AmeyMoreCode <amey.more024@gmail.com>
2023-06-13 21:11:54 +0530

    second

---
+++ src/cals.js
@@ -0,0 +1,8 @@
+
+
+function sum( n1, n2){
+    return n1+ n2;
+}
+//in js by default evevry member is private
+
+export { sum };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-console.log("amey");
+import{ sum } from "./cals.js";
+console.log("amey"); 
+
+let output=sum(10,20);
+console.log(output);
