9e508a4c40fc9aef0bc13ba39b7a972a34cdfa4b
AmeyMoreCode <amey.more024@gmail.com>
2023-06-13 22:29:43 +0530

    3done

--- src/cals.js
+++ src/cals.js
@@ -5,4 +5,11 @@ function sum( n1, n2){
 }
 //in js by default evevry member is private
 
-export { sum };
+
+
+function subtraction(n1,n2){
+    return n1-n2;
+}
+// export { sum };
+// export { subtraction };
+export { sum , subtraction};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import{ sum } from "./cals.js";
+// import{ sum } from "./cals.js";
+// import { subtraction } from "./cals.js";
+import { sum,subtraction } from "./cals";
+
 console.log("amey"); 
 
 let output=sum(10,20);
 console.log(output);
+
+let output1=subtraction(10,20);
+console.log(output1);
